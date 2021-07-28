35c258e5c3a9b634af83a27083112751b98d5956
inapeace <56222478+penglingss@users.noreply.github.com>
2021-07-28 16:55:50 +0900

    Create 괄호.java
    BOJ 9012번 괄호

---
+++ BOJ/[9012] 괄호/penglingss/괄호.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        int cnt;
+        for (int i = 0; i < n; i++) {
+            cnt = 0;
+            String str = arr[i];
+            for (int j = 0; j < str.length(); j++) {
+                if (cnt < 0) {
+                    break;
+                }
+
+                if (str.charAt(j) == '(') {
+                    cnt++;
+                } else if (str.charAt(j) == ')') {
+                    cnt--;
+                }
+            }
+            if (cnt == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
