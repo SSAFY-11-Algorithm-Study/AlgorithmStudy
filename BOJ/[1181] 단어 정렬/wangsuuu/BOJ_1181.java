5d191894795fb3dc9936466dfefaf7cfe886b8ac
wangsuuu <57534919+wangsuuu@users.noreply.github.com>
2021-08-07 23:46:22 +0900

    다시 올바르게 푼 코드
    Revise : BOJ1181 코드 수정

--- BOJ/[1181] 단어 정렬/wangsuuu/BOJ_1181.java
+++ BOJ/[1181] 단어 정렬/wangsuuu/BOJ_1181.java
@@ -1,7 +1,10 @@
+package week1;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class BOJ_1181 {
@@ -20,37 +23,30 @@ public class BOJ_1181 {
 		//입력 완료
 		
 		
-		//길이 순으로 정렬 : Comparator 활용
-		//Comparator(o1, o2) 리턴값 : 음수(오름차순 정렬), 0(값이 같음), 양수(내림차순)
-		
 		Arrays.sort(arr, new Comparator<String>() {
+
 			@Override
 			public int compare(String o1, String o2) {
-				return o1.length() - o2.length(); // 오름차순
-				//리턴값이 양수라면 o1>o2이고, 왼쪽이 크기 때문에 오른쪽으로 보내짐.
-				//음수라면 o1<o2이고, 그대로 작은 수가 왼쪽에 있게 됨.
-				// return o2.length() - o1.length() 이면 내림차순
 				
-				//로직 정확히는 모르곘음..
+				if(o1.length()==o2.length())
+					return o1.compareTo(o2);
+				else
+					return o1.length()-o2.length();
 			}
 		});
+		//정렬된 array 완성
 		
-		ArrayList<String> sorted = new ArrayList<>();
-		for(String str:arr) {
-			sorted.add(str);
+		//중복값 제외하고 차례대로 넣음
+		List<String> result = new ArrayList<>();
+		
+		for(String str : arr) {
+			if(!result.contains(str))
+				result.add(str);
 		}
 		
-		//중복값 제거
-		for (int i = 1; i < sorted.size(); i++) {
-			if(sorted.get(i).equals(sorted.get(i-1)))
-				sorted.remove(i-1);
-					
-		}
-		//글자 수가 같을 경우 그 안에서 다시 정렬 필요
+		for(String s: result)
+			System.out.println(s);
 		
-		for(String str : sorted) {
-				System.out.println(str);
-		}
 		
 	}
 }
