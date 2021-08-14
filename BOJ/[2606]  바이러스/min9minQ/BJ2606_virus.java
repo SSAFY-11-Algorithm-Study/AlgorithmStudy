a8cdf3fcde2f004332ab41c166344dc239ef204a
min9minQ <87810314+min9minQ@users.noreply.github.com>
2021-08-14 18:05:26 +0900

    Revised : 08.15 / [바이러스]
    알고리즘 3주차

--- BOJ/[2606]  바이러스/min9minQ/BJ2606_virus.java
+++ BOJ/[2606]  바이러스/min9minQ/BJ2606_virus.java
@@ -17,31 +17,31 @@ public class BJ2606_virus {
 		int link = Integer.parseInt(br.readLine());
 		int count = 0;
 		
-		ArrayList<Integer>[] virus = new ArrayList[num+1];
-		boolean[] isSelected = new boolean[num+1];
+		ArrayList<Integer>[] virus = new ArrayList[num+1];//ArrayList로 이루어진 배열 선언 (컴퓨터 인덱스 1부터로 맞추기 위해서 +1)
+		boolean[] isSelected = new boolean[num+1];// 연결되어 있으면 true로 표시 하기 위한 배열
 		for(int i = 0; i < num+1; i++) {
-			virus[i] = new ArrayList<>();
+			virus[i] = new ArrayList<>();//Arraylist 배열 각각 초기화
 		}
 		
 		for(int i = 0; i < link; i ++) {
 			st = new StringTokenizer(br.readLine());
 			int num1= Integer.parseInt(st.nextToken());
 			int num2= Integer.parseInt(st.nextToken());
-			virus[num1].add(num2);
-			virus[num2].add(num1);
+			virus[num1].add(num2);//num1에 num2연결 표시
+			virus[num2].add(num1);//num2에 num1 연결 표시
 		}
 		
-		search(1,isSelected,virus);
+		search(1,isSelected,virus);// DFS 형식의 search함수 호출
 		for(int i = 0; i < num+1; i++) {
-			if(isSelected[i]==true) {
+			if(isSelected[i]==true) {//true 갯수 카운팅.
 				count++;
 			}
 		}
 		
-		System.out.println(count-1);
+		System.out.println(count-1);//본인 빼기 위한 -1
 		
 		
-		/*
+		/*이 코드는 연결 입력 첫줄은 처음 무조건 1이 포함한 (1 2 처럼)컴퓨터를 데이터로 주는 줄 알고 짠 코드... set사용
 		HashSet<Integer> set = new HashSet<>();
 		set.add(1);
 		
@@ -60,7 +60,7 @@ public class BJ2606_virus {
 	private static void search(int num, boolean[] isSelected,ArrayList<Integer>[] virus) {
 		isSelected[num] = true;
 		for(int v : virus[num]) {
-			if(isSelected[v]==false)
+			if(isSelected[v]==false)// 이미 선택되었으면 안가도 됨. 이미 찾으러 떠났음.
 				search(v,isSelected,virus);
 		}
 		
