2e860d80c1cfd4ae0b0f16790d8c3668085db14d
alstjdwo1601 <48443234+alstjdwo1601@users.noreply.github.com>
2021-08-21 16:44:06 +0900

    Upload : 08.22 / [수열]
    알고리즘 4주차

---
+++ BOJ/[2559] 수열/alstjdwo1601/BOJ2559_수열.java
@@ -0,0 +1,32 @@
+package week4;
+
+import java.util.Scanner;
+
+public class BOJ2559_수열 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); // 전체 날짜
+		int K = sc.nextInt(); // 연속 날짜
+		
+		int []arr = new int[N];
+		int answer = Integer.MIN_VALUE;
+		
+		
+		for(int i=0; i<N; i++) 
+			arr[i] = sc.nextInt();
+		
+		
+		for ( int i=0; i<=N-K; i++) {
+			int sum =0;
+			for ( int j=i; j<i+K; j++) {
+				sum += arr[j];
+			}
+			answer = Math.max(answer, sum);
+		}
+		
+		System.out.println(answer);
+	}
+}
