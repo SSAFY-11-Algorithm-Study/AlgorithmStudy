990d6f86c7bfa1bd113131483e3d12ba5e085016
dain <56222478+penglingss@users.noreply.github.com>
2021-09-02 20:04:33 +0900

    Upload : 09.05 / [랜선 자르기]

---
+++ BOJ/[1615] 랜선 자르기/penglingss/랜선자르기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        long N = sc.nextInt();
+        long[] lan = new long[K];
+        long max = -1;
+
+        for (int i = 0; i < K; i++) {
+            lan[i] = sc.nextInt();
+            max = Math.max(max, lan[i]); // 최댓값 구하기
+        }
+
+        long min = 1; // 랜선의 길이는 
+
+        while (min <= max) { // 이분 탐색
+            long length = (max + min) / 2;
+            int cnt = 0;
+            for (int i = 0; i < K; i++) {
+                cnt += lan[i] / length;
+            }
+            if (cnt >= N) min = length + 1;
+            else max = length - 1;
+        }
+
+        System.out.println(max);
+    }
+}
