fe15fa18e3cd71c864a203fbd8968b076e11c3c8
ryuna <59449215+anottrx@users.noreply.github.com>
2021-09-09 23:08:25 +0900

    Upload : 09.12 / [등수 매기기]
    알고리즘 7주차

---
+++ BOJ/[2012] 등수 매기기/anottrx/등수매기기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] score = new int[N];
+        for (int i = 0; i < N; i++) { // 입력
+            score[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(score); // 정렬
+        long diff = 0;
+        for (int i = 0; i < N; i++) { // 차례대로 차이 구해서 합하기
+            diff = diff + (long) Math.abs(i + 1 - score[i]);
+        }
+        System.out.println(diff);
+    }
+}
