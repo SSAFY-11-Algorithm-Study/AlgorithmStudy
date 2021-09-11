ae162ecc48e7e1661f851cc6ffd590cb39886690
min <alstjdwo1601@naver.com>
2021-09-11 16:36:16 +0900

    Upload : 09.12 / [메뉴리뉴얼]
    알고리즘 7주차

---
+++ Programmers/[72411] 메뉴 리뉴얼/alstjdwo1601/PROG_메뉴리뉴얼.java
@@ -0,0 +1,78 @@
+package week7;
+
+import java.util.*;
+
+// 제가 이해한대로는 예시 2번에서 xyz 가 두개있으면 xz,xy같은것도
+// 정답에 포함되어야하는거같은데... 이해가 안가네요
+class Solution {
+    
+    static char[] arr ;
+    static char[] selected ; 
+    static Map<String, Integer> menuHM;
+    public String[] solution(String[] orders, int[] course) {
+        
+        menuHM = new HashMap<>(); //뽑은 조합을 중복없이 담을 해시맵
+                
+        //course 안에 갯수만큼 조합을 뽑아야됨
+        for (int i = 0; i < course.length; i++) { 
+            for (int j = 0; j < orders.length; j++) {
+                //정렬을 미리해둠
+                arr= orders[j].toCharArray();
+                Arrays.sort(arr); 
+                
+                //cousre[i] 개 만큼 조합뽑아서 selected에 저장
+                selected = new char[course[i]];
+                comb(0, 0, course[i]); 
+            }
+        }
+        
+        ArrayList<String> answerList = new ArrayList<>();
+        
+        //해시맵에 ([AB], 3) 이런식으로 담겨있으니 
+        //value가 2보다 같거나크면서 course[i]와 같으면 정답배열추가
+        for(int i = 0 ; i < course.length; i++){
+            for(Map.Entry<String, Integer> e : menuHM.entrySet()){
+                if(e.getValue() >=2 && e.getKey().length() == course[i]){
+                    answerList.add(e.getKey());
+                }
+            }
+        }
+        
+        String[] answer = new String[answerList.size()];
+        
+        for(int i = 0 ; i < answerList.size(); i ++){
+            answer[i] = answerList.get(i);
+        }
+                                    
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+    
+    //메뉴 조합뽑기
+    public static void comb(int depth , int start , int R){
+        if(depth == R ){
+            //System.out.println(Arrays.toString(selected));
+            
+            //[A,B]  -> AB라는 문자열로 해쉬에 담음
+            String str = "";
+            for(int i = 0 ; i < selected.length; i ++){
+                str += selected[i];
+            }
+            //System.out.println(str);
+            
+            //str이 있으면 value +1 해주고 아니면 1로 넣어줌
+            if(menuHM.containsKey(str))
+					menuHM.replace(str, menuHM.get(str) +1);
+			else 
+				menuHM.put(str, 1);
+            
+            return;
+        }
+        
+        for(int i = start ; i < arr.length ; i++){
+            selected[depth] = arr[i];
+            comb(depth+1, i+1, R);
+        }
+    }
+}
