877fa5245b12e366cc25c40abd69008c2aac0e99
dain <56222478+penglingss@users.noreply.github.com>
2021-09-17 14:57:44 +0900

    Upload : 09.19 / [조이스틱]

---
+++ Programmers/[42860] 조이스틱/pengligss/조이스틱.java
@@ -0,0 +1,29 @@
+// "ABABAAAAAB", "ABBAA" 통과 못하는 코드
+
+class Solution {
+    public int solution(String name) {
+
+        int ans = 0;
+        for(int i = 0 ; i < name.length() ; i++) {
+            if(name.charAt(i)!= 'A') {
+                int up = name.charAt(i) - 'A';
+                int down = 1 + 'Z' - name.charAt(i);
+                ans += Math.min(up, down);
+            }
+        }
+
+        int minMove = name.length() - 1;
+        for(int i = 0 ; i < name.length() ; i++) {
+            if(name.charAt(i) != 'A') {
+                int next = i+1;
+                while(next < name.length() && name.charAt(next) == 'A') {
+                    next++;
+                }
+                int move = 2 * i + name.length() - next;
+                minMove = Math.min(move, minMove);
+            }
+        }
+
+        return ans + minMove;
+    }
+}
