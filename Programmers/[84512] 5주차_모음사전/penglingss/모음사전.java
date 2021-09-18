99fa52f29e8763c9712d9d0407cd6b2b9f724276
dain <56222478+penglingss@users.noreply.github.com>
2021-09-18 23:25:09 +0900

    Revised : 09.19 / [5주차_모음사전]

--- Programmers/[84512] 5주차_모음사전/penglingss/모음사전.java
+++ Programmers/[84512] 5주차_모음사전/penglingss/모음사전.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String word) {
         int answer = 0;
         String aeiou = "AEIOU";
-        int[] th = {781, 156, 31, 6, 1};
+        int[] th = {781, 156, 31, 6, 1}; // 각 자리가 바뀌는데 {781, 156, 31, 6, 1} 만큼이 필요
         for(int i = 0; i < word.length(); i++) {
             int n = aeiou.indexOf(word.charAt(i));
             answer += th[i] * n;
