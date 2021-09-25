dbdad36105a107428f288dd17f381f8b34bff963
ryuna <59449215+anottrx@users.noreply.github.com>
2021-09-25 16:37:59 +0900

    Revised : 09.19 / [큰 수 만들기]
    알고리즘 8주차

--- Programmers/[42883] 큰 수 만들기/anottrx/큰수만들기.java
+++ Programmers/[42883] 큰 수 만들기/anottrx/큰수만들기.java
@@ -1,25 +1,34 @@
-// 여러 블로그 참고했습니다
-
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
-		int max = 0;
-		int idx = 0;
-		for (int i = 0; i < number.length() - k; i++) {
-			max = 0;
-			for (int j = idx; j <= i + k; j++) {
-				if ((number.charAt(j) - '0') > max) {
-					max = number.charAt(j) - '0';
-					idx = j + 1;
-				}
-			}
-			sb.append(String.valueOf(max));
-		}
-		answer = sb.toString();
-		if(answer.charAt(0)=='0') {
-			answer = answer.substring(1);
-		}
+
+        int idx = 0;
+        for (int i = 0; i < number.length() - k; i++) {
+            int max = 0;
+            for (int j = idx; j <= i + k; j++) {
+                if ((number.charAt(j) - '0') > max) {
+                    max = number.charAt(j) - '0';
+                    idx = j + 1;
+                    if (max == 9) { // 9라면 무조건 max
+                        break; // 시간 줄이기
+                    }
+                }
+            }
+            sb.append(String.valueOf(max));
+        }
+        answer = sb.toString();
+
+        int cntZero = 0;
+        for (int i = 0; i < answer.length(); i++) { // 앞자리가 0인지 확인하고 있다면 없애기
+            if (answer.charAt(i) == '0') {
+                cntZero++;
+            } else {
+                break;
+            }
+        }
+        answer = (answer.length() == cntZero) ? "0" : answer.substring(cntZero); // 0으로만 이루어졌다면 0 하나 출력
+
         return answer;
     }
 }
