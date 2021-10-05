827a297f067442e170f215ad4f4aee22c401343c
ryuna <59449215+anottrx@users.noreply.github.com>
2021-10-05 23:17:46 +0900

    Revised : 10.10 / [벽 부수고 이동하기]
    알고리즘 11주차

--- BOJ/[2206] 벽 부수고 이동하기/anottrx/벽부수고이동하기.java
+++ BOJ/[2206] 벽 부수고 이동하기/anottrx/벽부수고이동하기.java
@@ -1,5 +1,3 @@
-package BOJ;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -42,14 +40,14 @@ public class BOJ2206 {
         boolean[][][] visited = new boolean[2][N][M]; // 방문 여부 체크. 0은 벽 안 부쉈고, 1은 벽 부수고 이동
 
         Queue<Position> q = new LinkedList<>();
-        q.add(new Position(0, 0, 0, 0)); // 시작
+        q.add(new Position(0, 0, 0, 1)); // 시작
         visited[0][0][0] = true;
 
         while (!q.isEmpty()) {
             Position cur = q.poll();
 
             if (cur.x == N - 1 && cur.y == M - 1) { // 종료 조건
-                return cur.cnt + 1;
+                return cur.cnt;
             }
 
             for (int i = 0; i < 4; i++) {
@@ -64,9 +62,8 @@ public class BOJ2206 {
                     visited[wall][dx][dy] = true;
                     q.add(new Position(dx, dy, wall, cnt + 1));
                 } else if (map[dx][dy] == 1 && wall == 0) { // 1이라면 벽을 부수지 않은 경우만 이동
-                    wall++; // 벽 부쉈으니 1로 만들기
                     visited[wall][dx][dy] = true;
-                    q.add(new Position(dx, dy, wall, cnt + 1));
+                    q.add(new Position(dx, dy, wall + 1, cnt + 1));
                 }
             }
         }
