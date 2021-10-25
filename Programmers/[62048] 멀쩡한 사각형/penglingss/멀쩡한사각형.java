f73b749625fd7a28cf7e1d806afe0f2971c1d0c9
dain <56222478+penglingss@users.noreply.github.com>
2021-10-25 20:48:49 +0900

    Upload : 10.31 / [멀쩡한 사각형]

---
+++ Programmers/[62048] 멀쩡한 사각형/penglingss/멀쩡한사각형.java
@@ -0,0 +1,8 @@
+class Solution {
+	public long solution(int w,int h) {
+		long answer = 0;
+        for(int i = 0; i < w; i++)
+            answer += (Long.valueOf(h) * i) / Long.valueOf(w);
+		return answer * 2;
+	}
+}
