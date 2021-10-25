233b8442bc2de7314d54162b5ea43b93b877266d
ryuna <59449215+anottrx@users.noreply.github.com>
2021-10-25 20:01:11 +0900

    Revised : 10.24 / [불!]
    알고리즘 13주차

--- BOJ/[4179] 불!/anottrx/불.java
+++ BOJ/[4179] 불!/anottrx/불.java
@@ -6,111 +6,101 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ4179 {
-	static int R, C;
-	static char[][] map;
-	static boolean[][] visited;
-	static int[][] d = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+    static int R, C;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[][] d = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
 
-	static class Position {
-		int x, y;
+    static class Position {
+        int x, y;
 
-		public Position(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		map = new char[R][C];
-		visited = new boolean[R][C]; // 방문 체크. 지훈이와 불 모두 함께 사용
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char[R][C];
+        visited = new boolean[R][C]; // 방문 체크. 지훈이와 불 모두 함께 사용
 
-		boolean canEscapeNow = false; // 시작부터 끝날 수 있는지 확인
-		Queue<Position> Jihoon = new LinkedList<>(); // 지훈이 위치
-		Queue<Position> fires = new LinkedList<>(); // 불 위치
+        Queue<Position> Jihoon = new LinkedList<>(); // 지훈이 위치
+        Queue<Position> fires = new LinkedList<>(); // 불 위치
 
-		for (int i = 0; i < R; i++) {
-			map[i] = br.readLine().toCharArray();
-			for (int j = 0; j < C; j++) {
-				if (map[i][j] == 'J') {
-					if (i == 0 || i == R - 1 || j == 0 || j == C - 1) {
-						canEscapeNow = true; // 시작부터 벽에 붙어있다면 즉시 탈출 가능
-					}
-					Jihoon.add(new Position(i, j)); // 지훈이 위치 넣기
-					map[i][j] = '.'; // 지훈이 위치 .으로 바꾸기
-					visited[i][j] = true; // 방문 체크
-				} else if (map[i][j] == 'F') {
-					fires.add(new Position(i, j)); // 불 위치 넣기
-					visited[i][j] = true; // 방문 체크
-				}
-			}
-		}
+        for (int i = 0; i < R; i++) {
+            map[i] = br.readLine().toCharArray();
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] == 'J') {
+                    Jihoon.add(new Position(i, j)); // 지훈이 위치 넣기
+                    map[i][j] = '.'; // 지훈이 위치 .으로 바꾸기
+                    visited[i][j] = true; // 방문 체크
+                } else if (map[i][j] == 'F') {
+                    fires.add(new Position(i, j)); // 불 위치 넣기
+                    visited[i][j] = true; // 방문 체크
+                }
+            }
+        }
+        System.out.println(escape(Jihoon, fires));
+    }
 
-		if (canEscapeNow) { // 시작부터 탈출 가능하다면 1 출력하고 끝
-			System.out.println(1);
-		} else { // 그 외의 경우는 bfs로 탐색 시작하기
-			System.out.println(escape(Jihoon, fires));
-		}
-	}
+    private static String escape(Queue<Position> jihoon, Queue<Position> fires) {
+        int cnt = 0;
+        boolean isFinished = false;
 
-	private static String escape(Queue<Position> jihoon, Queue<Position> fires) {
-		int cnt = 0;
-		boolean isFinished = false;
+        while (!jihoon.isEmpty()) {
+            moveFire(fires); // 불부터 이동시키기
+            isFinished = moveJihoon(jihoon); // 지훈이 이동시키기
 
-		while (!jihoon.isEmpty()) {
-			cnt++; // 시간 증가
+            if (isFinished) { // true라면 탈출 성공
+                return String.valueOf(cnt + 1); // 벽에서 밖으로 나가는 것 때문에 1 추가해서 리턴
+            }
 
-			moveFire(fires); // 불부터 이동시키기
-			isFinished = moveJihoon(jihoon); // 지훈이 이동시키기
+            cnt++; // 시간 증가
+        }
 
-			if (isFinished) { // true라면 탈출 성공
-				return String.valueOf(cnt + 1); // 벽에서 밖으로 나가는 것 때문에 1 추가해서 리턴
-			}
-		}
+        return "IMPOSSIBLE"; // 여기까지 오면 탈출 못했기 때문에 IMPOSSIBLE 리턴
+    }
 
-		return "IMPOSSIBLE"; // 여기까지 오면 탈출 못했기 때문에 IMPOSSIBLE 리턴
-	}
+    private static void moveFire(Queue<Position> fires) { // 불 이동시키기. bfs
+        int size = fires.size();
 
-	private static void moveFire(Queue<Position> fires) { // 불 이동시키기. bfs
-		int size = fires.size();
+        for (int i = 0; i < size; i++) { // 불 큐 사이즈만큼 반복
+            Position cur = fires.poll();
 
-		for (int i = 0; i < size; i++) { // 불 큐 사이즈만큼 반복
-			Position cur = fires.poll();
+            for (int j = 0; j < 4; j++) {
+                int dx = cur.x + d[j][0];
+                int dy = cur.y + d[j][1];
+                if (dx >= 0 && dx < R && dy >= 0 && dy < C && map[dx][dy] == '.' && !visited[dx][dy]) {
+                    map[dx][dy] = 'F';
+                    fires.add(new Position(dx, dy));
+                    visited[dx][dy] = true;
+                }
+            }
+        }
+    }
 
-			for (int j = 0; j < 4; j++) {
-				int dx = cur.x + d[j][0];
-				int dy = cur.y + d[j][1];
-				if (dx >= 0 && dx < R && dy >= 0 && dy < C && map[dx][dy] == '.' && !visited[dx][dy]) {
-					map[dx][dy] = 'F';
-					fires.add(new Position(dx, dy));
-					visited[dx][dy] = true;
-				}
-			}
-		}
-	}
+    private static boolean moveJihoon(Queue<Position> jihoon) { // 지훈 이동시키기. bfs
+        int size = jihoon.size();
 
-	private static boolean moveJihoon(Queue<Position> jihoon) { // 지훈 이동시키기. bfs
-		int size = jihoon.size();
+        for (int i = 0; i < size; i++) { // 지훈이 큐 사이즈만큼 반복
+            Position cur = jihoon.poll();
+            if (cur.x == 0 || cur.x == R - 1 || cur.y == 0 || cur.y == C - 1) { // 미로의 가장자리에 접한 공간에 도달했다면
+                return true; // true 리턴해서 끝내기
+            }
 
-		for (int i = 0; i < size; i++) { // 지훈이 큐 사이즈만큼 반복
-			Position cur = jihoon.poll();
-
-			for (int j = 0; j < 4; j++) {
-				int dx = cur.x + d[j][0];
-				int dy = cur.y + d[j][1];
-				if (dx >= 0 && dx < R && dy >= 0 && dy < C && map[dx][dy] == '.' && !visited[dx][dy]) {
-					if (dx == 0 || dx == R - 1 || dy == 0 || dy == C - 1) { // 미로의 가장자리에 접한 공간에 도달했다면
-						return true; // true 리턴해서 끝내기
-					} else {
-						jihoon.add(new Position(dx, dy));
-						visited[dx][dy] = true;
-					}
-				}
-			}
-		}
-		return false; // 벽에 도착 못했다면 false 리턴
-	}
+            for (int j = 0; j < 4; j++) {
+                int dx = cur.x + d[j][0];
+                int dy = cur.y + d[j][1];
+                if (dx >= 0 && dx < R && dy >= 0 && dy < C && map[dx][dy] == '.' && !visited[dx][dy]) {
+                    jihoon.add(new Position(dx, dy));
+                    visited[dx][dy] = true;
+                }
+            }
+        }
+        return false; // 벽에 도착 못했다면 false 리턴
+    }
 }
