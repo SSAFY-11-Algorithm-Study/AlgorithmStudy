79d59732080b803ff518d52da08382a9239b80e3
ryuna <59449215+anottrx@users.noreply.github.com>
2021-11-07 13:52:42 +0900

    Revised : 11.07 / [단어 변환]
    알고리즘 15주차

--- Programmers/[43163] 단어 변환/anottrx/단어변환.java
+++ Programmers/[43163] 단어 변환/anottrx/단어변환.java
@@ -1,72 +1,49 @@
-// bfs로 안되길래 다시 dfs로 시도는 했는데, 단어 알파벳 차이 개수 세는 것 자체가 이상해서
-// https://mungto.tistory.com/53 이곳 참고해서 좀더 직관적으로 다시 풀어볼 예정입니다ㅠ
-
-public class PROG43163 {
+class Solution {
 
     static int min;
 
-    public static void main(String[] args) {
-        String begin = "hit";
-        String target = "cog";
-        String[] words = { "hot", "dot", "dog", "lot", "log", "cog" };
-
+    public int solution(String begin, String target, String[] words) {
         int answer = 0;
 
         min = words.length + 1;
         boolean[] visited = new boolean[words.length];
 
-        findWord(begin, -1, 0, words, target, visited);
+        findWord(begin, 0, words, target, visited);
 
         answer = min;
-        if (answer == words.length + 1) {
+        if (answer == words.length + 1) { // 변환할 수 없는 경우에는 0을 리턴
             answer = 0;
         }
-        System.out.println(answer);
-        // return answer;
+
+        return answer;
     }
 
-    private static void findWord(String curWord, int x, int cnt, String[] words, String target, boolean[] visited) {
-        if (x == words.length) {
+    private static void findWord(String curWord, int cnt, String[] words, String target, boolean[] visited) {
+        if (curWord.equals(target)) { // targert과 같아지면 최솟값 구하기
+            min = Math.min(min, cnt);
             return;
         }
 
-        for (int i = x + 1; i < words.length; i++) {
-            if (!visited[i]) {
-                int diffCnt = countDiff(curWord, words[i], target);
-                if (diffCnt == 0) {
-                    min = Math.min(min, cnt);
-                    break;
-                } else if (diffCnt > 0) {
-                    visited[i] = true;
-                    findWord(words[i], i, cnt + 1, words, target, visited);
-                }
+        for (int i = 0; i < words.length; i++) {
+            if (!visited[i] && countDiff(curWord, words[i])) {
+                visited[i] = true;
+                findWord(words[i], cnt + 1, words, target, visited);
+                visited[i] = false;
             }
         }
-
     }
 
-    private static int countDiff(String curWord, String nextWord, String target) {
-        int nth = 0, len = curWord.length(), sameTarget = 0;
-        boolean flag = false;
+    private static boolean countDiff(String curWord, String nextWord) { // 중복되는 알파벳 개수 세기
+        int len = curWord.length(), sameCnt = 0;
 
         for (int i = 0; i < len; i++) {
-            if (!flag && curWord.charAt(i) != nextWord.charAt(i)) {
-                if (nextWord.charAt(i) == target.charAt(i)) {
-                    nth = i;
-                    flag = true;
+            if (curWord.charAt(i) != nextWord.charAt(i)) {
+                sameCnt++;
+                if (sameCnt > 1) {
+                    return false; // 다른 알파벳이 2개 이상이라면 false
                 }
             }
-            if (nextWord.charAt(i) == target.charAt(i)) {
-                sameTarget++;
-            }
         }
-
-        if (sameTarget == len) {
-            return 0;
-        }
-        if (nth == 0) {
-            return -1;
-        }
-        return nth;
+        return true; // 중복되는 단어는 없기 때문에 여기로 오면 알파벳 1개만 다른 것
     }
 }
