435cdc92b8293feb441d89440d71d3e2382e1316
dain <56222478+penglingss@users.noreply.github.com>
2021-11-17 22:11:54 +0900

    Upload : 11.21 / [타일 채우기]

---
+++ BOJ/[2133] 타일 채우기/penglingss/타일채우기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+
+        for (int i = 2; i < N + 1; i = i + 2) {
+            dp[i] = dp[i - 2] * 3;
+            for (int j = 0; j < i - 2; j = j + 2) {
+                dp[i] = dp[i] + dp[j] * 2;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
