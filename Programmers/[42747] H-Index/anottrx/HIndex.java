c67432bfc7a8afd47fffe06306fd83c165acff52
ryuna <59449215+anottrx@users.noreply.github.com>
2021-12-23 18:42:05 +0900

    Revised : 12.23 / [H-Index]
    알고리즘 21주차

--- Programmers/[42747] H-Index/anottrx/HIndex.java
+++ Programmers/[42747] H-Index/anottrx/HIndex.java
@@ -1,16 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
 
-        // citations 정렬할 필요 없다
+        Arrays.sort(citations);
         int cur = citations[citations.length - 1]; // 답은 citations 배열 안에 없을 수도 있다
         finding: for (int i = cur; i >= 0; i--) {
             int cnt = 0;
             for (int j = citations.length - 1; j >= 0; j--) {
-                if (citations[j] >= cur) {
+                if (citations[j] >= cur) { // 인용된 논문 횟수 세기
                     cnt++;
                 }
-                if (cnt >= cur && cur >= (citations.length - cnt + 1)) {
+                if (cnt >= cur && cur >= (citations.length - cnt + 1)) { // h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용
                     answer = cur; // 최댓값
                     break finding;
                 }
