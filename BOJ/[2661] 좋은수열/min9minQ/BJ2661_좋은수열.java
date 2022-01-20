f6db7ea92f346d8b8b616645e3a05cd40fcb4eb2
이민규 <aaasssddd123@naver.com>
2022-01-20 17:56:00 +0900

    Upload : 01.20 / [좋은수열]
    알고리즘 24주차

---
+++ BOJ/[2661] 좋은수열/min9minQ/BJ2661_좋은수열.java
@@ -0,0 +1,54 @@
+package time24;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BJ2661_좋은수열 {
+	
+	private static int N;
+	private static boolean flag = false;
+	private static ArrayList<String> result = new ArrayList<>();
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		
+		back("");
+		
+		Collections.sort(result);
+		
+		System.out.println(result.get(0));
+
+	}
+
+	private static void back(String number) {
+		
+		if(number.length() == N) {
+			result.add(number);
+			return;
+		}
+		
+		for(int i = 1; i <= 3; i++) {
+			if(isvalid(number+i)) {
+				back(number+i);
+			}
+		}
+	}
+
+	private static boolean isvalid(String temp) {
+		int length = temp.length();
+		
+		for(int i = 1; i <= length/2; i++) {
+			String front = temp.substring(temp.length()-2*i, temp.length()-i);
+			String back = temp.substring(temp.length()-i, temp.length());
+			
+			if(front.equals(back)) return false;
+		}
+		return true;
+	}
+
+}
+ 
