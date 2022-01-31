9e83ca4af5d84c944a19f06124f2b7945ef47a45
ryuna <59449215+anottrx@users.noreply.github.com>
2022-01-31 23:32:54 +0900

    Revised : 01.27 / [신입 사원]
    알고리즘 25주차

--- BOJ/[1946] 신입 사원/anottrx/신입사원.java
+++ BOJ/[1946] 신입 사원/anottrx/신입사원.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ1946 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
@@ -16,17 +16,17 @@ public class Main {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int score = Integer.parseInt(st.nextToken());
                 int nth = Integer.parseInt(st.nextToken());
-                scores[nth - 1] = score;
+                scores[nth - 1] = score; // 면접 등수 순서대로 점수 입력
             }
-            int nth = 1;
-            int nthScore = scores[0];
+            int cnt = 1; // 우선 면접 1등은 무조건 선발할 것
+            int bestScore = scores[0]; // 면접 1등의 서류심사 성적
             for (int i = 0; i < N; i++) {
-                if (scores[i] < nthScore) {
-                    nth++;
-                    nthScore = scores[i];
+                if (scores[i] < bestScore) { // 면접 1등의 서류 성적보다 더 높다면
+                    cnt++; // 이 사람도 뽑을 테니 선발 인원 1 증가
+                    bestScore = scores[i]; // 서류 성적 바꾸기
                 }
             }
-            System.out.println(nth);
+            System.out.println(cnt);
         }
     }
 }
