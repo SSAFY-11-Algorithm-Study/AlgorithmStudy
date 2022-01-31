80ac2fc379f8dbb6601def1943a4cac72321f0fe
ryuna <59449215+anottrx@users.noreply.github.com>
2022-01-31 23:26:46 +0900

    Revised : 01.27 / [등수 구하기]
    알고리즘 25주차

--- BOJ/[1205] 등수 구하기/anottrx/등수구하기.java
+++ BOJ/[1205] 등수 구하기/anottrx/등수구하기.java
@@ -1,13 +1,12 @@
-// https://velog.io/@dydgjs2016/%EB%B0%B1%EC%A4%80-1205-%EB%93%B1%EC%88%98-%EA%B5%AC%ED%95%98%EA%B8%B0%EA%B5%AC%ED%98%84 참고했습니다
+package BOJ;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ1205 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,7 +15,7 @@ public class Main {
         int P = Integer.parseInt(st.nextToken()); // 랭킹 리스트에 올라 갈 수 있는 점수의 개수
 
         int answer = 1;
-        if (N != 0) {
+        if (N != 0) { // N이 0이라면 무조건 1이 답
             st = new StringTokenizer(br.readLine());
             ArrayList<Integer> arr = new ArrayList<>();
             for (int i = 0; i < N; i++) {
@@ -26,15 +25,9 @@ public class Main {
             if (N == P && newScore <= arr.get(arr.size() - 1)) {
                 answer = -1;
             } else {
-                Collections.sort(arr);
-                for (int i = arr.size() - 1; i >= 0; i--) {
-                    if (arr.get(i) > newScore) {
+                for (int i = 0; i < arr.size(); i++) {
+                    if (arr.get(i) > newScore) { // 태수의 새 점수보다 높다면 등수 1씩 더하기
                         answer++;
-                    } else {
-                        // if (arr.get(i) == newScore && answer >= P) {
-                        // answer = -1;
-                        // }
-                        break;
                     }
                 }
             }
