15a38139d59043ec2b5f7a55350d6a18ed4be184
SuRuen <wsr5683@naver.com>
2022-03-03 18:41:27 +0900

    Upload : 03.03 / [문자열 압축]

---
+++ Programmers/[60057] 문자열 압축/wangsuuu/PRO_Compression.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 100000;
+
+
+        for(int i=1; i<=s.length()/2; i++){
+
+            int idx = 0;
+            String ans = "";
+
+            while(idx<=s.length()){
+
+                if(idx+i > s.length()) {
+                    ans += s.substring(idx);
+                    break;
+                }
+
+                String st = s.substring(idx, idx+i); //비교가 되는 기준
+                int num=1;
+                while(idx+2*i<=s.length() && s.substring(idx+i, idx+2*i).equals(st)){
+                    num++; idx++;
+                }
+
+                if(num==1){
+                    ans += st;
+                    idx += i;
+                } else{
+                    ans += num + st;
+                    idx = idx+2*i-1;
+                }
+            }
+            answer = Math.min(answer, ans.length());
+        }
+
+        return answer;
+    }
+}
