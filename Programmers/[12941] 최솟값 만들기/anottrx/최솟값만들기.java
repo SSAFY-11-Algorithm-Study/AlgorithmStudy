249f11da8e9cb641a704429ac39ad2c837f70da6
ryuna <anottrx@gmail.com>
2022-05-06 23:37:41 +0900

    Upload : 05.12 / [최솟값 만들기]

---
+++ Programmers/[12941] 최솟값 만들기/anottrx/최솟값만들기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[A.length - i - 1];
+        }
+
+        return answer;
+    }
+}
